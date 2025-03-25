fa4b0f6a20bcf512821fefa1753f6cc4bb6ad449
Nadia Viñabal <nadiavinabal@hotmail.com>
2025-03-25 14:45:21 -0300

    fix: modify JWT configuration

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -4,15 +4,15 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import config from 'config';
 import { ConfigType } from '@nestjs/config';
 import { PayloadToken } from '../models/token.model'; 
-
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor(@Inject(config.KEY) private configService: ConfigType<typeof config>) {
+    constructor(private configService: ConfigService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: configService.jwt.secret || 'secret',
+            secretOrKey: configService.get<string>('JWT_SECRET'),
         });
     }
 
